89b1389ffdf71eebca9f45d71448366d35607b53
Himanshu Reddy <60777610+himanshu-r@users.noreply.github.com>
2021-06-06 09:46:23 +0530

    Completely Uploaded

---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js Bootcamp</h1>
+      <p>This was an amzing bootcamp taken up by Shaurya Shina</p>
+    </div>
+  );
+}
+
+export default Note;
